bd40da588503bac96b3a28387b508446d94ee50c
Blu89 <mattbarron89@gmail.com>
2022-01-24 15:47:47 -0500

    Add data access project to solution

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
 {
     class Program
     {
-        static void Main(string[] args)
+        private void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+            Console.WriteLine("Type 'help' to list available coffee shop commands, " +
+                              "type 'quit' to exit application.");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+            var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops().ToList();
+
+            var line = string.Empty;
+
+            while (!string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+            {
+                line = Console.ReadLine();
+
+                var commandHandler =
+                    string.Equals("help", line, StringComparison.OrdinalIgnoreCase)
+                        ? new HelpCommandHandler(coffeeShops) as ICommandHandler
+                        : new CoffeeShopCommandHandler(coffeeShops, line);
+
+                commandHandler.HandleCommands();
+            }
         }
     }
 }
